a83a4a988c36c0d72fe5c08183d9877442053e53
Muhammad Hanif Amrullah Fakhruddin <hamexhanif@yahoo.com>
2022-11-28 23:35:16 +0100

    added test for add methode and repaired add methode

--- demo/src/test/java/com/example/AppTest.java
+++ demo/src/test/java/com/example/AppTest.java
@@ -5,14 +5,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- * Unit test for simple App.
+ * Unit test for SimpleLinkedList Class.
  */
 class AppTest {
-    /**
-     * Rigorous Test.
-     */
-    @Test
-    void testApp() {
-        assertEquals(1, 1);
-    }
+	@Test
+	void testAdd() {
+		SimpleLinkedList<Integer> list = new SimpleLinkedList<Integer>(); //create empty list
+		assertEquals(list.size(), 0); //check size of list, should equal 0
+		list.add(3); //add 3 into list 
+		assertEquals(list.size(), 1); //check size of list, should equal 1
+		list.add(5);//add 5 into list
+		assertEquals(list.size(), 2); //check size of list, should equal 2
+	}
 }
