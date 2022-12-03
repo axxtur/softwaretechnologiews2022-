ac8707be9f28dc008af795e84f2256307eddf8fc
Muhammad Hanif Amrullah Fakhruddin <hamexhanif@yahoo.com>
2022-12-03 09:36:24 +0100

    implement remove method and it's test

--- demo/src/test/java/com/example/AppTest.java
+++ demo/src/test/java/com/example/AppTest.java
@@ -3,6 +3,9 @@ package com.example;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Iterator;
 
 /**
  * Unit test for SimpleLinkedList Class.
@@ -17,4 +20,55 @@ class AppTest {
 		list.add(5);//add 5 into list
 		assertEquals(list.size(), 2); //check size of list, should equal 2
 	}
+	
+	@Test
+    public void testRemoveEmpty() {    	
+    	SimpleLinkedList<Integer> list = new SimpleLinkedList<>();
+        Iterator<Integer> iter = list.iterator();
+        list.printList();
+        System.out.println(" <- printed empty list\n");
+        
+        Exception exception = assertThrows(NullPointerException.class, () ->
+        iter.remove());
+        
+        assertEquals("List is empty, cannot delete element", exception.getMessage());
+//        try {
+//        	iter.remove();	
+//        }catch(Exception e) {
+//        	System.out.println(e.getMessage());
+//        }
+    }
+    
+    @Test
+    public void testRemoveWithOneElement() {
+        SimpleLinkedList<Integer> list = new SimpleLinkedList<>();
+        Iterator<Integer> iter = list.iterator();        
+        list.add(1);
+        list.printList();
+        System.out.println("<- printed list with one element\n");
+        assertEquals(1, list.size()); //check size of list, should equal 1        
+        iter.remove();
+        list.printList();
+        System.out.println("<- printed list with removed first element");
+        assertEquals(0, list.size()); //check size of list, should equal 0
+    }
+      
+    @Test
+    public void testRemoveWithTwoElement() {
+        SimpleLinkedList<Integer> list = new SimpleLinkedList<>();
+        Iterator<Integer> iter = list.iterator();        
+        list.add(1);
+        list.printList();
+        System.out.println("<- printed list with one element\n");
+        assertEquals(1, list.size()); //check size of list, should equal 1
+        list.add(2);
+        list.printList();
+        System.out.println("<- printed list with two element\\n");
+        assertEquals(2, list.size()); //check size of list, should equal 2
+        
+        iter.remove();
+        list.printList();
+        System.out.println("<- printed list with removed first element");
+        assertEquals(1, list.size()); //check size of list, should equal 1
+    }
 }
